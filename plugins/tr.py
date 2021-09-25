15b6e82511609f315c74e0c4469e8536205d08a6
anonymous <89799912+PRATHEEsni@users.noreply.github.com>
2021-09-25 13:19:12 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -1,7 +1,7 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
-START_TEXT = '👋 `Hey My Name Is` **VcVideoPlayer** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`💡 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
-DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"
+HELP_TEXT =f"**SETTINGS** \n\n`1) Add This Bot To Your Group And Make It Admin` \n2) **Add** @{ASSISTANT_NAME} **To Your Group** \n\n **COMMANDS** \n\n- `/stream` **: Reply To Video Or File That You Want To Stream In Vc.** \n- `/stopstream`  **: Stop The Stream**"
+START_TEXT = 'Hᴇʏ Mʏ Nᴀᴍᴇ Is Eʟɪɴᴀ 💖 [ ](https://telegra.ph/file/703f85ad4c629f6c07b12.jpg) \n\n Lᴇᴛs Eɴᴊᴏʏ Cɪɴᴇᴍᴀᴛɪᴄ Vɪᴇᴡ Oғ Gʀᴏᴜᴘ Vɪᴅᴇᴏ Pʟᴀʏᴇʀ Wɪᴛʜ Yᴏᴜʀ Fʀɪᴇɴᴅs ✨❤️ \n\n 𝐑𝐞𝐠𝐚𝐫𝐝𝐬 🥀 : @Pratheek_XD'
+ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n🤖 This Bot Was Created To Stream Video In Telegram Group Video Chats Using Several Methods From Webrtc.` \n\n💡 POWERED BY PYTGCALLS THE ASYNC CLIENT API FOR THE TELEGRAM GROUP CALLS, AND PYROGRAM THE TELEGRAM MTPROTO API CLIENT LIBRARY AND FRAMEWORK IN PURE PYTHON FOR USERS AND BOTS. \n\nTHIS BOT LICENSED UNDER GNU-GPL 3.0 LICENSE'
+DEVS_TEXT =f"𝗘𝗟𝗜𝗡𝗔 𝗩𝗖 𝗣𝗟𝗔𝗬𝗘𝗥 \n\nHere Some Developers Helping In Making The Eʟɪɴᴀ 🇮🇳 Bot."
